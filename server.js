bc4a06aa31fba331d23723124b40dc483a4244a0
hype1356 <swayamkan@gmail.com>
2021-03-22 15:45:10 +0000

    small change not much

--- server.js
+++ server.js
@@ -34,87 +34,94 @@ var server = http.createServer(function (req, res) {
     initi: try {
       console.log(cookies);
       if (!(allowedpeople.includes(cookies[1]))) {
-        res.statusCode = 302;
+        console.log("redirect")
+        res.statusCode = 301;
         res.setHeader('Location', '/login');
+        proceeed = false
         res.end();
       } else {break initi;}
     } catch {
-      res.statusCode = 302;
+      console.log("redirect2")
+      res.statusCode = 301;
       res.setHeader('Location', '/login');
+      proceeed = false
       res.end();
     }
   }
 
-  switch (req.url){
-    default:
-      let cookies = req.headers.cookie;
-      res.statusCode = 302;
-      try {
-        cookies = cookies.split('=');
-      } catch {
-        console.log('no cookie');
-      } finally {
+  if (proceeed == true) {
+    switch (req.url){
+      /*
+      default:
+        let cookies = req.headers.cookie;
+        res.statusCode = 301;
         try {
-          console.log(cookies);
-          if (allowedpeople.includes(cookies[1])) {
-            res.setHeader('Location', '/home');
-          } else {
-            res.setHeader('Location', '/login');
-          }
-        } catch {res.setHeader('Location', '/login');}
-      }
-      res.end();
-      break;
-    
-    case '/home':
-      res.statusCode = 200;
-      res.setHeader('Content-Type', 'text/html');
-      sendfile('home.html', res);
-      break;
-
-    case '/login':
-      res.statusCode = 200;
-      res.setHeader('Content-Type', 'text/html');
-      sendfile('login.html', res);
-      break;
-
-    case '/login/action':
-      req.setEncoding('utf-8');
-      req.on('data', chunk => {
-        data = chunk.split("&");
-        n = data[0].split('=')[1]; 
-        p = data[1].split('=')[1];
-        console.log(n, p);
-        res.statusCode = 302;
-        if (allowedpeople.includes(n)) {
-          if (accdetail[n] == p) {
-            res.setHeader('Set-Cookie', cookie.serialize('player', n));
-            res.setHeader('Location', '/home')
-          } else {
-            res.setHeader('Location', '/login');
-          }
-        } else {
-          res.setHeader('Location', '/login');
+          cookies = cookies.split('=');
+        } catch {
+          console.log('no cookie');
+        } finally {
+          try {
+            console.log(cookies);
+            if (allowedpeople.includes(cookies[1])) {
+              res.setHeader('Location', '/home');
+            } else {
+              res.setHeader('Location', '/login');
+            }
+          } catch {res.setHeader('Location', '/login');}
         }
-        res.end;
-      });
+        res.end();
+        break;
+      */
+      case '/home':
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'text/html');
+        sendfile('home.html', res);
+        break;
 
-    case '/play':
-      res.statusCode = 200;
-      res.setHeader('Content-Type', 'text/html');
-      sendfile("html.html", res);
-      break;
+      case '/login':
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'text/html');
+        sendfile('login.html', res);
+        break;
 
-    case '/play/css':
-      res.setHeader('Content-Type', 'text/css');
-      sendfile('cs.css', res);
-      break;
+      case '/login/action':
+        req.setEncoding('utf-8');
+        req.on('data', chunk => {
+          data = chunk.split("&");
+          n = data[0].split('=')[1]; 
+          p = data[1].split('=')[1];
+          console.log(n, p);
+          res.statusCode = 302;
+          if (allowedpeople.includes(n)) {
+            if (accdetail[n] == p) {
+              res.setHeader('Set-Cookie', cookie.serialize('player', n));
+              res.setHeader('Location', '/home')
+            } else {
+              res.setHeader('Location', '/login');
+            }
+          } else {
+            res.setHeader('Location', '/login');
+          }
+          res.end;
+        });
 
-    case '/play/js':
-      res.setHeader('Content-Type', 'text/js');
-      sendfile('cod.js', res);
-      break;
-  }
+      case '/play':
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'text/html');
+        sendfile("html.html", res);
+        break;
+
+      case '/play/css':
+        res.setHeader('Content-Type', 'text/css');
+        sendfile('cs.css', res);
+        break;
+
+      case '/play/js':
+        res.setHeader('Content-Type', 'text/js');
+        sendfile('cod.js', res);
+        break;
+    }
+  }  
 })
 
 server.listen(port, hostname, () => { //this is what gets printed to the _console_.
